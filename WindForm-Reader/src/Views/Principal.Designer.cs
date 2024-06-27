3a4744c31448f01ca6d4097015c24e2792c50e92
Javi Quintana <rikiquintana7@gmail.com>
2024-06-27 13:44:28 -0500

    Terminacion del Winform

--- WindForm-Reader/src/Views/Principal.Designer.cs
+++ WindForm-Reader/src/Views/Principal.Designer.cs
@@ -41,6 +41,12 @@
             DataGridFacturas = new DataGridViewTextBoxColumn();
             btnLimpiarPagas = new Button();
             btnLimpiarNoPagas = new Button();
+            bntPregunta1 = new Button();
+            LabelPreguntas = new Label();
+            bntPregunta2 = new Button();
+            btnPregunta3 = new Button();
+            btnPregunta4 = new Button();
+            btnPregunta5 = new Button();
             menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)DataGridFactura).BeginInit();
             SuspendLayout();
@@ -51,7 +57,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { MenuAbrir, MenuMas });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(714, 32);
+            menuStrip1.Size = new Size(714, 26);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -60,27 +66,27 @@
             MenuAbrir.DropDownItems.AddRange(new ToolStripItem[] { MenuItemRegistrar, MenuItemGuardar, MenuItemCargar });
             MenuAbrir.Font = new Font("Algerian", 8F, FontStyle.Italic, GraphicsUnit.Point, 0);
             MenuAbrir.Name = "MenuAbrir";
-            MenuAbrir.Size = new Size(71, 28);
+            MenuAbrir.Size = new Size(71, 22);
             MenuAbrir.Text = "Abrir";
             // 
             // MenuItemRegistrar
             // 
             MenuItemRegistrar.Name = "MenuItemRegistrar";
-            MenuItemRegistrar.Size = new Size(270, 34);
+            MenuItemRegistrar.Size = new Size(267, 34);
             MenuItemRegistrar.Text = "Registrar Factura";
             MenuItemRegistrar.Click += toolStripMenuItem2_Click;
             // 
             // MenuItemGuardar
             // 
             MenuItemGuardar.Name = "MenuItemGuardar";
-            MenuItemGuardar.Size = new Size(270, 34);
+            MenuItemGuardar.Size = new Size(267, 34);
             MenuItemGuardar.Text = "Guardar Facturas";
             MenuItemGuardar.Click += MenuItemGuardar_Click;
             // 
             // MenuItemCargar
             // 
             MenuItemCargar.Name = "MenuItemCargar";
-            MenuItemCargar.Size = new Size(270, 34);
+            MenuItemCargar.Size = new Size(267, 34);
             MenuItemCargar.Text = "Cargar Facturas";
             MenuItemCargar.Click += MenuItemCargar_Click;
             // 
@@ -89,7 +95,7 @@
             MenuMas.DropDownItems.AddRange(new ToolStripItem[] { MenuItemSalir });
             MenuMas.Font = new Font("Algerian", 8F, FontStyle.Italic, GraphicsUnit.Point, 0);
             MenuMas.Name = "MenuMas";
-            MenuMas.Size = new Size(57, 28);
+            MenuMas.Size = new Size(57, 22);
             MenuMas.Text = "Mas";
             // 
             // MenuItemSalir
@@ -103,7 +109,7 @@
             // 
             etiTitulo.AutoSize = true;
             etiTitulo.Font = new Font("Showcard Gothic", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            etiTitulo.Location = new Point(71, 50);
+            etiTitulo.Location = new Point(12, 50);
             etiTitulo.Name = "etiTitulo";
             etiTitulo.Size = new Size(273, 35);
             etiTitulo.TabIndex = 9;
@@ -117,10 +123,10 @@
             DataGridFactura.CellBorderStyle = DataGridViewCellBorderStyle.Sunken;
             DataGridFactura.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             DataGridFactura.Columns.AddRange(new DataGridViewColumn[] { DataGridFacturas });
-            DataGridFactura.Location = new Point(38, 123);
+            DataGridFactura.Location = new Point(12, 123);
             DataGridFactura.Name = "DataGridFactura";
             DataGridFactura.RowHeadersWidth = 62;
-            DataGridFactura.Size = new Size(363, 449);
+            DataGridFactura.Size = new Size(499, 388);
             DataGridFactura.TabIndex = 10;
             // 
             // DataGridFacturas
@@ -137,9 +143,9 @@
             // 
             btnLimpiarPagas.Cursor = Cursors.Hand;
             btnLimpiarPagas.Font = new Font("Algerian", 8F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            btnLimpiarPagas.Location = new Point(421, 538);
+            btnLimpiarPagas.Location = new Point(268, 537);
             btnLimpiarPagas.Name = "btnLimpiarPagas";
-            btnLimpiarPagas.Size = new Size(281, 34);
+            btnLimpiarPagas.Size = new Size(250, 34);
             btnLimpiarPagas.TabIndex = 11;
             btnLimpiarPagas.Text = "Limpiar Facturas Pagas";
             btnLimpiarPagas.UseVisualStyleBackColor = true;
@@ -149,19 +155,90 @@
             // 
             btnLimpiarNoPagas.Cursor = Cursors.Hand;
             btnLimpiarNoPagas.Font = new Font("Algerian", 8F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            btnLimpiarNoPagas.Location = new Point(421, 464);
+            btnLimpiarNoPagas.Location = new Point(12, 537);
             btnLimpiarNoPagas.Name = "btnLimpiarNoPagas";
-            btnLimpiarNoPagas.Size = new Size(281, 34);
+            btnLimpiarNoPagas.Size = new Size(250, 34);
             btnLimpiarNoPagas.TabIndex = 12;
             btnLimpiarNoPagas.Text = "Limpiar Facturas No Pagas";
             btnLimpiarNoPagas.UseVisualStyleBackColor = true;
             btnLimpiarNoPagas.Click += btnLimpiarNoPagas_Click;
+            // 
+            // bntPregunta1
+            // 
+            bntPregunta1.Font = new Font("Algerian", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            bntPregunta1.Location = new Point(517, 123);
+            bntPregunta1.Name = "bntPregunta1";
+            bntPregunta1.Size = new Size(184, 34);
+            bntPregunta1.TabIndex = 13;
+            bntPregunta1.Text = "Pregunta 1";
+            bntPregunta1.UseVisualStyleBackColor = true;
+            bntPregunta1.Click += bntPregunta1_Click;
+            // 
+            // LabelPreguntas
+            // 
+            LabelPreguntas.AutoSize = true;
+            LabelPreguntas.Font = new Font("Showcard Gothic", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LabelPreguntas.Location = new Point(517, 50);
+            LabelPreguntas.Name = "LabelPreguntas";
+            LabelPreguntas.Size = new Size(184, 35);
+            LabelPreguntas.TabIndex = 14;
+            LabelPreguntas.Text = "PREGUNTAS";
+            // 
+            // bntPregunta2
+            // 
+            bntPregunta2.Font = new Font("Algerian", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            bntPregunta2.Location = new Point(517, 203);
+            bntPregunta2.Name = "bntPregunta2";
+            bntPregunta2.Size = new Size(184, 34);
+            bntPregunta2.TabIndex = 15;
+            bntPregunta2.Text = "Pregunta 2";
+            bntPregunta2.UseVisualStyleBackColor = true;
+            bntPregunta2.Click += bntPregunta2_Click;
+            // 
+            // btnPregunta3
+            // 
+            btnPregunta3.Font = new Font("Algerian", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnPregunta3.Location = new Point(517, 291);
+            btnPregunta3.Name = "btnPregunta3";
+            btnPregunta3.Size = new Size(184, 34);
+            btnPregunta3.TabIndex = 16;
+            btnPregunta3.Text = "Pregunta 3";
+            btnPregunta3.UseVisualStyleBackColor = true;
+            btnPregunta3.Click += btnPregunta3_Click;
+            // 
+            // btnPregunta4
+            // 
+            btnPregunta4.Font = new Font("Algerian", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnPregunta4.Location = new Point(517, 384);
+            btnPregunta4.Name = "btnPregunta4";
+            btnPregunta4.Size = new Size(184, 34);
+            btnPregunta4.TabIndex = 17;
+            btnPregunta4.Text = "Pregunta 4";
+            btnPregunta4.UseVisualStyleBackColor = true;
+            btnPregunta4.Click += btnPregunta4_Click;
+            // 
+            // btnPregunta5
+            // 
+            btnPregunta5.Font = new Font("Algerian", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnPregunta5.Location = new Point(517, 477);
+            btnPregunta5.Name = "btnPregunta5";
+            btnPregunta5.Size = new Size(184, 34);
+            btnPregunta5.TabIndex = 18;
+            btnPregunta5.Text = "Pregunta 5";
+            btnPregunta5.UseVisualStyleBackColor = true;
+            btnPregunta5.Click += btnPregunta5_Click;
             // 
             // Principal
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(714, 609);
+            Controls.Add(btnPregunta5);
+            Controls.Add(btnPregunta4);
+            Controls.Add(btnPregunta3);
+            Controls.Add(bntPregunta2);
+            Controls.Add(LabelPreguntas);
+            Controls.Add(bntPregunta1);
             Controls.Add(btnLimpiarNoPagas);
             Controls.Add(btnLimpiarPagas);
             Controls.Add(DataGridFactura);
@@ -191,5 +268,11 @@
         private DataGridViewTextBoxColumn DataGridFacturas;
         private Button btnLimpiarPagas;
         private Button btnLimpiarNoPagas;
+        private Button bntPregunta1;
+        public Label LabelPreguntas;
+        private Button bntPregunta2;
+        private Button btnPregunta3;
+        private Button btnPregunta4;
+        private Button btnPregunta5;
     }
 }
--- WindForm-Reader/src/Views/Principal.cs
+++ WindForm-Reader/src/Views/Principal.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using WindForm_Reader.src.Services;
+using static WindForm_Reader.src.Services.Return;
 
 namespace WindForm_Reader.src.Views
 {
@@ -7,12 +8,17 @@ namespace WindForm_Reader.src.Views
     {
         Return _return = new Return();
 
-        public bool CargueOK { get; set; } = false;
+        private List<string> ListaFacuras = new List<string>();
+        private List<string> FacturasDuplicadas = new List<string>();
+
+        private bool CargueOK { get; set; } = false;
+        private int Cargados {  get; set; }
+        private string Codigo {  get; set; }
 
         public Principal()
         {
             InitializeComponent();
-            this.CenterToScreen();
+            CenterToScreen();
         }
 
         private void toolStripMenuItem6_Click(object sender, EventArgs e) => this.Dispose();
@@ -172,32 +178,243 @@ namespace WindForm_Reader.src.Views
                     string[] lines = File.ReadAllLines(_OpenFile.FileName);
 
                     foreach (string line in lines)
-                    {
-                        if (_return.IsCargue_OK(line)) 
+                    {                       
+                        if (_return.IsCargue_OK(line))
                         {
-                            AgregarFacturaDesdeTexto(line);  
-                            CargueOK = true;
-                        } 
+                            if (IsNotCargue_Duplicated(line))
+                            {
+                                AgregarFacturaDesdeTexto(line);
+                                CargueOK = true;
+                                Cargados++;
+                            }
+                            else
+                            {
+                                FacturasDuplicadas.Add(line);
+                            }
+                        }
                     }
 
-                    if (CargueOK) MessageBox.Show("Archivo cargado exitosamente.",
-                                                  "Éxito",
-                                                  MessageBoxButtons.OK,
-                                                  MessageBoxIcon.Information);
+                    if (FacturasDuplicadas.Count > 1)
+                    {
+                        string facturasTexto = string.Join("\n", FacturasDuplicadas);
+
+                        MessageBox.Show($"Facturas Duplicadas no cargadas: {facturasTexto}\n",
+                                         "Diplicado",
+                                         MessageBoxButtons.OK,
+                                         MessageBoxIcon.Warning);
+                    }
+
+                    if (CargueOK) MessageBox.Show($"Facturas cargadas exitosamente. {Cargados}",
+                                                   "Cargue",
+                                                   MessageBoxButtons.OK,
+                                                   MessageBoxIcon.Information);
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show($"Ocurrió un error al cargar el archivo: {ex.Message}", 
-                                    "Error", 
-                                    MessageBoxButtons.OK, 
+                    MessageBox.Show($"Ocurrió un error al cargar el archivo: {ex.Message}",
+                                    "Error",
+                                    MessageBoxButtons.OK,
                                     MessageBoxIcon.Error);
 
                     throw;
                 }
+
+                FacturasDuplicadas.Clear(); 
+                Cargados = 0;
             }
         }
 
         private void AgregarFacturaDesdeTexto(string facturaTexto) => DataGridFactura.Rows.Add(facturaTexto);
 
+        private void bntPregunta1_Click(object sender, EventArgs e)
+        {
+            DialogResult _Pregunta = MessageBox.Show("Cuáles son las facturas que fueron pagadas en el mes de Enero. \n\n" +
+                                                     "Ver Respuesta?",
+                                                     "Pregunta 1",
+                                                     MessageBoxButtons.YesNo,
+                                                     MessageBoxIcon.Question);
+            if (_Pregunta is DialogResult.Yes)
+            {
+                foreach (DataGridViewRow row in DataGridFactura.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        string factura = string.Join(";", row.Cells.Cast<DataGridViewCell>().Select(cell => cell.Value?.ToString() ?? ""));
+                        ListaFacuras.Add(factura);
+                    }
+                }
+
+                if (ListaFacuras.Count is 0)
+                {
+                    MessageBox.Show("No hay Facturas a leer para responder la pregunta 1",
+                                    "Advertencia",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                    return;
+                }
+
+                _return.Pregunta1(ListaFacuras);
+
+                ListaFacuras.Clear();
+            }
+        }
+
+        private void bntPregunta2_Click(object sender, EventArgs e)
+        {
+            DialogResult _Pregunta = MessageBox.Show("Cuál es el total de Facturas pagadas. \n\n" +
+                                                     "Ver Respuesta?",
+                                                     "Pregunta 2",
+                                                     MessageBoxButtons.YesNo,
+                                                     MessageBoxIcon.Question);
+            if (_Pregunta is DialogResult.Yes)
+            {
+                foreach (DataGridViewRow row in DataGridFactura.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        string factura = string.Join(";", row.Cells.Cast<DataGridViewCell>().Select(cell => cell.Value?.ToString() ?? ""));
+                        ListaFacuras.Add(factura);
+                    }
+                }
+
+                if (ListaFacuras.Count is 0)
+                {
+                    MessageBox.Show("No hay Facturas a leer para responder la pregunta 1",
+                                    "Advertencia",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                    return;
+                }
+
+                _return.Pregunta2(ListaFacuras);
+
+                ListaFacuras.Clear();
+            }
+        }
+
+        private void btnPregunta3_Click(object sender, EventArgs e)
+        {
+            DialogResult _Pregunta = MessageBox.Show("Cuál es el total de Facturas NO pagadas. \n\n" +
+                                                     "Ver Respuesta?",
+                                                     "Pregunta 3",
+                                                     MessageBoxButtons.YesNo,
+                                                     MessageBoxIcon.Question);
+            if (_Pregunta is DialogResult.Yes)
+            {
+                foreach (DataGridViewRow row in DataGridFactura.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        string factura = string.Join(";", row.Cells.Cast<DataGridViewCell>().Select(cell => cell.Value?.ToString() ?? ""));
+                        ListaFacuras.Add(factura);
+                    }
+                }
+
+                if (ListaFacuras.Count is 0)
+                {
+                    MessageBox.Show("No hay Facturas a leer para responder la pregunta 1",
+                                    "Advertencia",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                    return;
+                }
+
+                _return.Pregunta3(ListaFacuras);
+
+                ListaFacuras.Clear();
+            }
+        }
+
+        private void btnPregunta4_Click(object sender, EventArgs e)
+        {
+            DialogResult _Pregunta = MessageBox.Show("Cuáles fueron las facturas pagadas en el primer semestre => \n(enero,febrero,marzo,abril,mayo,junio). \n\n" +
+                                                     "Ver Respuesta?",
+                                                     "Pregunta 4",
+                                                     MessageBoxButtons.YesNo,
+                                                     MessageBoxIcon.Question);
+            if (_Pregunta is DialogResult.Yes)
+            {
+                foreach (DataGridViewRow row in DataGridFactura.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        string factura = string.Join(";", row.Cells.Cast<DataGridViewCell>().Select(cell => cell.Value?.ToString() ?? ""));
+                        ListaFacuras.Add(factura);
+                    }
+                }
+
+                if (ListaFacuras.Count is 0)
+                {
+                    MessageBox.Show("No hay Facturas a leer para responder la pregunta 1",
+                                    "Advertencia",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                    return;
+                }
+
+                _return.Pregunta4(ListaFacuras);
+
+                ListaFacuras.Clear();
+            }
+        }
+
+        private void btnPregunta5_Click(object sender, EventArgs e)
+        {
+            DialogResult _Pregunta = MessageBox.Show("Cuáles fueron las facturas pagadas en el segundo semestre => \n(julio,agosto,septiembre,octubre,noviembre,diciembre). \n\n" +
+                                                     "Ver Respuesta?",
+                                                     "Pregunta 5",
+                                                     MessageBoxButtons.YesNo,
+                                                     MessageBoxIcon.Question);
+            if (_Pregunta is DialogResult.Yes)
+            {
+                foreach (DataGridViewRow row in DataGridFactura.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        string factura = string.Join(";", row.Cells.Cast<DataGridViewCell>().Select(cell => cell.Value?.ToString() ?? ""));
+                        ListaFacuras.Add(factura);
+                    }
+                }
+
+                if (ListaFacuras.Count is 0)
+                {
+                    MessageBox.Show("No hay Facturas a leer para responder la pregunta 1",
+                                    "Advertencia",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                    return;
+                }
+
+                _return.Pregunta5(ListaFacuras);
+
+                ListaFacuras.Clear();
+            }
+        }
+
+        private bool IsNotCargue_Duplicated(string line)
+        {
+            string[] FacturaRecibida = line.Split(';');
+            if (_return.SetPosition(FacturaRecibida))
+            {
+                Codigo = _return._Modelo.NumeroFactura;
+            }
+
+            foreach (DataGridViewRow row in DataGridFactura.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    string factura = string.Join(";", row.Cells.Cast<DataGridViewCell>().Select(cell => cell.Value?.ToString() ?? ""));
+
+                    string[] position = factura.Split(';');
+
+                    if (_return.SetPosition(position))
+                    {
+                        if(_return._Modelo.NumeroFactura == Codigo) return false;
+                    }
+                }
+            }
+
+            return true;
+        }
     }
 }
